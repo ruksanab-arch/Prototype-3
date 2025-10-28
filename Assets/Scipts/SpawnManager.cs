9ff65c52a8da124324cea9487a07c014de4685e3
Ruksana <ruksana.b@indivillage.in>
2025-10-28 16:34:34 +0530

    Create new float variables for startDelay and repeatRate

--- Assets/Scipts/SpawnManager.cs
+++ Assets/Scipts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
     // Start is called before the first frame update
     void Start()
     {
