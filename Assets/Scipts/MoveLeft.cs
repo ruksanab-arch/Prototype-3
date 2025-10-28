d5659f5381c044da8a3926b92c3a1dd53aa6ac89
Ruksana <ruksana.b@indivillage.in>
2025-10-28 15:28:21 +0530

    Created a spawn manager

--- Assets/Scipts/MoveLeft.cs
+++ Assets/Scipts/MoveLeft.cs
@@ -16,6 +16,7 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     
     }
 }
--- Assets/Scipts/SpawnManager.cs
+++ Assets/Scipts/SpawnManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
     // Update is called once per frame
