cea4c3c297639e6f87b790047df36f1a4d01e18d
Ruksana <ruksana.b@indivillage.in>
2025-10-28 14:53:41 +0530

    Correction of script

--- Assets/Scipts/MoveLeft.cs
+++ Assets/Scipts/MoveLeft.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    
     private float speed = 30;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    
     }
 }
