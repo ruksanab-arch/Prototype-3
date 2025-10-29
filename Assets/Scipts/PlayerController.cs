b7d2e891218aad14cda5c7a8f58215b62f6d3352
Ruksana <ruksana.b@indivillage.in>
2025-10-29 13:24:11 +0530

    Set up a jump animation

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -10,11 +10,12 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver;
     private Animator playerAnim;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
 
     }
@@ -26,6 +27,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_tri");
         }
     }
 
