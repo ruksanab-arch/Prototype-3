cd0f0d11c7dad6506395ded5a6c790339bdd6d42
Ruksana <ruksana.b@indivillage.in>
2025-10-29 12:35:01 +0530

    Creating a variable leftBound

--- Assets/Scipts/MoveLeft.cs
+++ Assets/Scipts/MoveLeft.cs
@@ -8,6 +8,7 @@ public class MoveLeft : MonoBehaviour
 
     private float speed = 30;
     private PlayerController playerControllerScript;
+    private float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
