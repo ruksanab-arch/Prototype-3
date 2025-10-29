a9013eaaf2a80de89fefe7614ee8964ecbbb63f0
Ruksana <ruksana.b@indivillage.in>
2025-10-29 11:42:35 +0530

    Fixing the background repeat with collider

--- Assets/Scipts/RepeatBackground.cs
+++ Assets/Scipts/RepeatBackground.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
