5f1cc5fb2eb9326a1f16783a6af6730b95e1bdd7
Ruksana <ruksana.b@indivillage.in>
2025-10-29 12:04:20 +0530

    Adding a game over trigger when the player collides with an obstacle

--- Assets/Scipts/PlayerController.cs
+++ Assets/Scipts/PlayerController.cs
@@ -29,6 +29,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameobject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (ollision.gameobject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+        
     }
 }
