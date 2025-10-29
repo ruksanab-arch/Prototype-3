f23d44d10f707f74fa25ece317361cafe81a55f2
Ruksana <ruksana.b@indivillage.in>
2025-10-29 11:54:50 +0530

    Adding a “Ground” tag to the Ground and an “Obstacle” tag to the Obstacle prefab

--- Assets/Scipts/RepeatBackground.cs
+++ Assets/Scipts/RepeatBackground.cs
@@ -10,13 +10,14 @@ public class RepeatBackground : MonoBehaviour
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
     
-    if (transform.position.x < startPos.x -50)
+    if (transform.position.x < startPos.x -repeatWidth)
     {
             transform.position = startPos;
     }
